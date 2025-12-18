239706f152fc5cab78797a555f26bdfd55b62dca
sofiyasudhagar2024csbs <sofiyasudhagar.2024csbs@sece.ac.in>
2025-12-18 09:30:30 +0530

    first commit

--- Db/db.js
+++ Db/db.js
@@ -1,18 +1,13 @@
-//will handle connectivity between express and mongodb
-// for commmonjs => const mongoose = require('mongoose')
-import mongoose from "mongoose";    // modules
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-dotenv.config();  
-
-const connectDB = () =>{
-try{
-    //'mongodb://127.0.0.1:27017/sece_todo'
-    mongoose.connect(process.env.MONGODB_URI);
-     console.log("db has been connected")
-    } catch (err){console.log(err)}
-
-    }
-
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- index.js
+++ index.js
@@ -1,25 +1,2 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import todoRoutes from "./Routes/todoRoutes.js";
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// 👇 VERY IMPORTANT
-app.use("/csbs", todoRoutes);
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+import connectDB from "./Db/db.js";
+connectDB();
